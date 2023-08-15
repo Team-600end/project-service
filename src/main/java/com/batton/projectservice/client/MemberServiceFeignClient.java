4ce24a6603ae4833856fdd3a1ae78ddaa39618db
lsh9295 <lsh929500@gmail.com>
2023-08-15 22:40:12 +0900

    add: prometheus

--- src/main/java/com/batton/projectservice/client/MemberServiceFeignClient.java
+++ src/main/java/com/batton/projectservice/client/MemberServiceFeignClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "member-service", url = "http://localhost:8081")
+@FeignClient(name = "member-service", url = "http://10.0.19.219:8081")
 public interface MemberServiceFeignClient {
     @RequestMapping(method = RequestMethod.GET, value = "/members/{memberId}", consumes = "application/json")
     GetMemberResDTO getMember(@PathVariable("memberId") Long memberId);
